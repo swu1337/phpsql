2ab6aa81af9ce3bc64afb25840720e7a19b4c378
SWu <SWu>
2016-09-22 15:26:46 +0200

    last opgave

--- poll.php
+++ poll.php
@@ -13,6 +13,7 @@
             $query->execute();
             $result1 = $query->fetchAll(PDO::FETCH_ASSOC);
             foreach ($result1 as $data1) {
+                print_r($data1);
                 echo "<input type='radio' name='optie' value=" . $data1['id'] . " />" . $data1['optie'];
                 echo "<br />";
             }
@@ -23,7 +24,20 @@
         <?php
 
         if(isset($_POST['Submit'])) {
-            
+            print_r($_POST);
+            $id = filter_input(INPUT_POST, 'optie', FILTER_VALIDATE_INT);
+            echo "Selected  Value" . $id;
+            if(isset($id)) {
+                $query = $db->prepare("UPDATE optie SET stemmen=stemmen+1 WHERE id=" . $id);
+                echo print_r($query);
+                //$query->bindParam("id", $id);
+
+                if($query->execute()) {
+                    echo "Thank you for the survey";
+                } else {
+                    echo "Something went wrong";
+                }
+            }
         }
     } catch(PDOException $e) {
         echo 'Error! :' . $e.getMessage();
